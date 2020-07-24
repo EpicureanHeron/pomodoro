4b23d9c23301c74fe91aebc0cbdde11d07501d0d
epicureanheron <joethor1989@gmail.com>
2020-07-24 15:54:55 -0500

    clock counts down, stop button works, began on the CSS.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //1. display 25 minutes on the screen -- DONE
 //2. interval by seconds the 25 minutes -- DONE
-//3. stop button should work
+//3. stop button should work -- DOne
 //4. Perhaps include an API to encourage qutoes every 5 minutes
 //5. CSS 
 
@@ -14,6 +14,7 @@ var difference;
 var interval;
 var timerStarted = false;
 var timerPaused = false;
+var timerLength = 25
 
 $("#start").click(function () {
 	// alert("Handler for .click() called.");
@@ -27,17 +28,25 @@ $("#start").click(function () {
 
 	}
 
+	if(timerPaused == true){
+		startTimer()
+		timerPaused = false;
+	}
+
 });
 
 
 $("#stop").click(function () {
-	alert("Handler for .click() called.");
+	stopTimer()
 });
+
+
+
 
 
 function createTime() {
 	currentTime = moment()
-	twentyFiveMinFromNow = moment().add(25, 'm')
+	twentyFiveMinFromNow = moment().add(timerLength, 'm')
 
 	console.log(currentTime)
 	console.log(twentyFiveMinFromNow)
@@ -45,7 +54,7 @@ function createTime() {
 
 function stopTimer(){
 	clearInterval(interval)
-
+	timerPaused = true;
 }
 
 function startTimer() {
@@ -59,14 +68,16 @@ function count() {
 	twentyFiveMinFromNow = twentyFiveMinFromNow - 1000;
 
 	difference = twentyFiveMinFromNow - currentTime
-
-	if(difference == 0){
-		alert('CONGRATS! Good job')
-	}
-
 	displayTime = moment(difference).format("mm:ss")
 	
 	//updates the time left
 	$(".timer").html("Time Left: " + displayTime);
+
+	if(difference == 0){
+		alert('CONGRATS! Good job');
+		stopTimer();
+		timerStarted = false;
+		$(".timer").html("Time Left: " + displayTime);
+	}
 
 }
